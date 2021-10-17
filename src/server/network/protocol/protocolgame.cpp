67f5974f3d1447d0c39a9a499be01191e3a89725
Eduardo Dantas <eduardo.dantas@hotmail.com.br>
2021-10-17 15:33:29 -0300

    Add support to protocol 12.72

--- src/server/network/protocol/protocolgame.cpp
+++ src/server/network/protocol/protocolgame.cpp
@@ -2912,10 +2912,11 @@ void ProtocolGame::sendCyclopediaCharacterGeneralStats()
 	// loyalty bonus
 	msg.add<uint16_t>(player->getBaseMagicLevel());
 	msg.add<uint16_t>(player->getMagicLevelPercent() * 100);
+
+	// Check if all clients have the same hardcoded skill ids
+	static const uint8_t HardcodedSkillIds[] = { 11, 9, 8, 10, 7, 6, 13 };
 	for (uint8_t i = SKILL_FIRST; i < SKILL_CRITICAL_HIT_CHANCE; ++i)
 	{
-		// check if all clients have the same hardcoded skill ids
-		static const uint8_t HardcodedSkillIds[] = {11, 9, 8, 10, 7, 6, 13};
 		msg.addByte(HardcodedSkillIds[i]);
 		msg.add<uint16_t>(std::min<int32_t>(player->getSkillLevel(i), std::numeric_limits<uint16_t>::max()));
 		msg.add<uint16_t>(player->getBaseSkill(i));
@@ -2923,6 +2924,29 @@ void ProtocolGame::sendCyclopediaCharacterGeneralStats()
 		msg.add<uint16_t>(player->getBaseSkill(i));
 		msg.add<uint16_t>(player->getSkillPercent(i) * 100);
 	}
+
+	// Version 12.70 start
+	msg.addByte(0x00); // bool ?
+	msg.addByte(0xA1); // Unknown
+
+	msg.add<uint16_t>(player->getMagicLevel());
+	msg.add<uint16_t>(player->getBaseMagicLevel());
+	// Loyalty bonus
+	msg.add<uint16_t>(player->getBaseMagicLevel());
+	msg.add<uint16_t>(player->getMagicLevelPercent() * 100);
+
+	static const skills_t SkillsIterator[] = {SKILL_FIST, SKILL_CLUB, SKILL_SWORD, SKILL_AXE, SKILL_DISTANCE, SKILL_SHIELD, SKILL_FISHING, SKILL_CRITICAL_HIT_CHANCE, SKILL_LIFE_LEECH_CHANCE, SKILL_MANA_LEECH_CHANCE};
+	for (skills_t skill : SkillsIterator) {
+		msg.add<uint16_t>(std::min<int32_t>(player->getSkillLevel(static_cast<uint8_t>(skill)), std::numeric_limits<uint16_t>::max()));
+		msg.add<uint16_t>(player->getBaseSkill(static_cast<uint8_t>(skill)));
+		// Loyalty bonus
+		msg.add<uint16_t>(player->getBaseSkill(static_cast<uint8_t>(skill)));
+		msg.add<uint16_t>(player->getSkillPercent(static_cast<uint8_t>(skill)) * 100);
+	}
+
+	msg.add<uint32_t>(player->getCapacity());
+	msg.add<uint32_t>(player->getCapacity());
+	// Version 12.70 end
 	writeToOutputBuffer(msg);
 }
 
@@ -2937,6 +2961,23 @@ void ProtocolGame::sendCyclopediaCharacterCombatStats()
 		msg.add<uint16_t>(std::min<int32_t>(player->getSkillLevel(i), std::numeric_limits<uint16_t>::max()));
 		msg.add<uint16_t>(0);
 	}
+
+	// Version 12.70 start
+	msg.add<uint16_t>(0); // Cleave
+
+	// Magic shield capacity
+	msg.add<uint16_t>(0); // Direct bonus
+	msg.add<uint16_t>(0); // Percentage bonus
+
+	msg.add<uint16_t>(0); // Perfect shot range 1
+	msg.add<uint16_t>(0); // Perfect shot range 2
+	msg.add<uint16_t>(0); // Perfect shot range 3
+	msg.add<uint16_t>(0); // Perfect shot range 4
+	msg.add<uint16_t>(0); // Perfect shot range 5
+
+	msg.add<uint16_t>(0); // Reflection
+	// Version 12.70 end
+
 	uint8_t haveBlesses = 0;
 	uint8_t blessings = 8;
 	for (uint8_t i = 1; i < blessings; ++i)
@@ -2946,8 +2987,10 @@ void ProtocolGame::sendCyclopediaCharacterCombatStats()
 			++haveBlesses;
 		}
 	}
+
 	msg.addByte(haveBlesses);
 	msg.addByte(blessings);
+
 	const Item *weapon = player->getWeapon();
 	if (weapon)
 	{
@@ -3027,6 +3070,7 @@ void ProtocolGame::sendCyclopediaCharacterCombatStats()
 		msg.addByte(0);
 		msg.addByte(CIPBIA_ELEMENTAL_UNDEFINED);
 	}
+
 	msg.add<uint16_t>(player->getArmor());
 	msg.add<uint16_t>(player->getDefense());
 
@@ -3089,6 +3133,12 @@ void ProtocolGame::sendCyclopediaCharacterCombatStats()
 			++combats;
 		}
 	}
+
+	// Version 12.70
+	// This next byte is not realy before the 'msg.addByte(combats)'
+	// in fact this is the last one byte before sending!!
+	// PS: This byte represents a header for an array, that means with value '0' we dont need to continue sending bytes.
+	msg.addByte(0); // Unknown size
 
 	msg.setBufferPosition(startCombats);
 	msg.addByte(combats);
@@ -4483,6 +4533,47 @@ void ProtocolGame::sendMarketDetail(uint16_t itemId)
 		}
 		ss << " oz";
 		msg.addString(ss.str());
+	}
+	else
+	{
+		msg.add<uint16_t>(0x00);
+	}
+
+	// Version 12.70
+	// Magic
+	std::ostringstream string;
+	msg.add<uint16_t>(0x00);
+
+	// Cleave
+	if (it.abilities && it.abilities->cleaveDamage)
+	{
+		string.clear();
+		string << it.abilities->cleaveDamage << "%";
+		msg.addString(string.str());
+	}
+	else
+	{
+		msg.add<uint16_t>(0x00);
+	}
+
+	// Reflection
+	if (it.abilities && it.abilities->reflectDamage)
+	{
+		string.clear();
+		string << it.abilities->reflectDamage;
+		msg.addString(string.str());
+	}
+	else
+	{
+		msg.add<uint16_t>(0x00);
+	}
+
+	// Perf shot
+	if (it.abilities && it.abilities->perfectBonus)
+	{
+		string.clear();
+		string << "+" << it.abilities->perfectBonus << " at range";
+		msg.addString(string.str());
 	}
 	else
 	{
